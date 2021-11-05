4ee48e477a0fd977a83ef06bb460c7e10a9a41b3
LivyaSusilo-71190456 <89835737+LivyaSusilo-71190456@users.noreply.github.com>
2021-11-06 00:23:40 +0700

    pertemuan7_71190456
    pertemuan7_71190456

---
+++ pertemuan6_71190456/main/java/id/ac/ukdw/pertemuan6_71190456/LoginFragment.java
@@ -0,0 +1,6 @@
+package id.ac.ukdw.pertemuan6_71190456;
+
+import android.app.Activity;
+
+public class LoginFragment extends Activity {
+}
---
+++ pertemuan6_71190456/main/java/id/ac/ukdw/pertemuan6_71190456/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package id.ac.ukdw.pertemuan6_71190456.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
