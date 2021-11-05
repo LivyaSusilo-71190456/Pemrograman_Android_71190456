8a198e9bfdf117a1858643fc2205a9f7005b5baa
LivyaSusilo-71190456 <89835737+LivyaSusilo-71190456@users.noreply.github.com>
2021-11-06 00:29:00 +0700

    pertemuan8_71190456

---
+++ pertemuan8_71190456/main/java/id/ac/ukdw/pertemuan8_71190456/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package id.ac.ukdw.pertemuan8_71190456.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
