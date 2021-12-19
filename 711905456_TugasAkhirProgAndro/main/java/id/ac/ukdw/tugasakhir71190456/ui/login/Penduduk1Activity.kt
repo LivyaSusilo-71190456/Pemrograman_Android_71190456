81db618159d5e998311c47a45656bb4d3991cbde
LivyaSusilo-71190456 <89835737+LivyaSusilo-71190456@users.noreply.github.com>
2021-12-19 23:04:47 +0700

    penduduk
    tugas akhir progandro

---
+++ 711905456_TugasAkhirProgAndro/main/java/id/ac/ukdw/tugasakhir71190456/ui/login/Penduduk1Activity.kt
@@ -0,0 +1,15 @@
+package id.ac.ukdw.tugasakhir71190456.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.ukdw.tugasakhir71190456.R
+
+class penduduk : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_penduduk)
+
+
+
+    }
+}
